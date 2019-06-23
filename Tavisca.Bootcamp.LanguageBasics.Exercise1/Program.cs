316049f31a30a5262d1ea53d168abd0ea07f0928
tavisca-apriyadarshi <51911375+tavisca-apriyadarshi@users.noreply.github.com>
2019-06-23 23:24:18 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -20,9 +20,121 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"{args} : {result}");
         }
 
-        public static int FindDigit(string equation)
+        public static int FindDigit(string s)
         {
             // Add your code here.
+            
+            int r = -1;
+            int a=0, b=0, c=0;
+            String A="", B="", C="";
+            int strt = s.IndexOf("*");
+            int lst = s.IndexOf("=");
+            
+            //Distributing the no. in A B C
+            for(int i=s.Length-1; i>=0; i--){
+                if(s[i].Equals('=')){
+                    C = s.Substring(i+1);
+                }
+                if(s[i].Equals('*')){
+                    B = s.Substring(i+1, lst-strt-1);
+                }
+            }
+            A = s.Substring(0, strt);
+
+            //Converting A B C into no.
+            if(A.IndexOf('?')<0){
+                a = Int32.Parse(A);
+            }
+            if(B.IndexOf('?')<0){
+                b = Int32.Parse(B);
+            }
+            if(C.IndexOf('?')<0){
+                c = Int32.Parse(C);
+            }
+
+            //Checking for missing digit
+            if(C.IndexOf('?')>=0){
+                int ans = a*b;
+                String str = ans.ToString();
+                int pos = C.IndexOf('?');
+                char res = str[pos];
+                r = res - '0';
+
+                if(C.IndexOf('?')==0){
+                    if(str.Equals(C.Substring(1))){
+                        r=-1;
+                    }
+                }
+            }
+            if(A.IndexOf('?')>=0){
+                if(b != 0){
+                    int ans = c/b;
+                    String str = ans.ToString();
+                    int pos = A.IndexOf('?');
+                    char res = str[pos];
+                    r = res - '0';
+
+                    if(A.IndexOf('?')==0){
+                        if(str.Equals(A.Substring(1))){
+                        r=-1;
+                        }
+                    }
+                    if(A.IndexOf('?')==A.Length-1){
+                        if(str.Equals(A.Substring(0, A.Length-2))){
+                        r=-1;
+                        }else {
+                            String strA = A.Substring(0,A.Length-1);
+                            int Aa = Int32.Parse(strA);
+                            for(int x=0; x<10; x++){
+                                if(((Aa)*10+x)*b==c){
+                                    r = x;
+                                    break;
+                                }
+                                if((Aa*10+x)*b!=c){
+                                    r=-1;
+                                }
+                            }
+                        }
+                    }
+                }else{
+                    r=-1;
+                }
+            }
+            if(B.IndexOf('?')>=0){
+                if(a != 0){
+                    int ans = c/a;
+                    String str = ans.ToString();
+                    int pos = B.IndexOf('?');
+                    char res = str[pos];
+                    r = res - '0';
+
+                    if(B.IndexOf('?')==0){
+                        if(str.Equals(B.Substring(1))){
+                        r=-1;
+                        }
+                    }
+                    if(B.IndexOf('?')==B.Length-1){
+                        if(str.Equals(B.Substring(0, B.Length-2))){
+                        r=-1;
+                        }else {
+                            String strB = B.Substring(0,B.Length-1);
+                            int Bb = Int32.Parse(strB);
+                            for(int x=0; x<10; x++){
+                                if(((Bb)*10+x)*a==c){
+                                    r = x;
+                                    break;
+                                }
+                                if((Bb*10+x)*a!=c){
+                                    r=-1;
+                                }
+                            }
+                        }
+                    }
+                }else{
+                    r=-1;
+                }
+            }
+            return r;
             throw new NotImplementedException();
         }
     }
