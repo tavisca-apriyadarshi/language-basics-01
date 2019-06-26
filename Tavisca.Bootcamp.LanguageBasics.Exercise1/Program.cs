536838de05045655b827f10b7d26e079795f6054
tavisca-apriyadarshi <apriyadarshi@tavisca.in>
2019-06-26 10:53:37 +0530

    updating Q1 for Submission of A2

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -20,122 +20,142 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"{args} : {result}");
         }
 
-        public static int FindDigit(string s)
-        {
-            // Add your code here.
+        public static int FindDigit(String Equation){
+            int MissingNumber = -1;
+            int FirstNumber=0, SecondNumber=0, MultiplicationResult=0;
+            String FirstNumberString="", SecondNumberString="", MultiplicationResultString="";
+            int MultiplicationPosition = Equation.IndexOf("*");
+            int equalsPosition = Equation.IndexOf("=");
             
-            int r = -1;
-            int a=0, b=0, c=0;
-            String A="", B="", C="";
-            int strt = s.IndexOf("*");
-            int lst = s.IndexOf("=");
-            
-            //Distributing the no. in A B C
-            for(int i=s.Length-1; i>=0; i--){
-                if(s[i].Equals('=')){
-                    C = s.Substring(i+1);
-                }
-                if(s[i].Equals('*')){
-                    B = s.Substring(i+1, lst-strt-1);
-                }
-            }
-            A = s.Substring(0, strt);
+            MultiplicationResultString = Equation.Substring(equalsPosition+1);
+            SecondNumberString = Equation.Substring(MultiplicationPosition+1, equalsPosition-MultiplicationPosition-1);
+            FirstNumberString = Equation.Substring(0, MultiplicationPosition);
 
-            //Converting A B C into no.
-            if(A.IndexOf('?')<0){
-                a = Int32.Parse(A);
+            //Converting FirstNumberString SecondNumberString MultiplicationResultString into no.
+            if(FirstNumberString.IndexOf('?')<0){
+                FirstNumber = Int32.Parse(FirstNumberString);
             }
-            if(B.IndexOf('?')<0){
-                b = Int32.Parse(B);
+            if(SecondNumberString.IndexOf('?')<0){
+                SecondNumber = Int32.Parse(SecondNumberString);
             }
-            if(C.IndexOf('?')<0){
-                c = Int32.Parse(C);
+            if(MultiplicationResultString.IndexOf('?')<0){
+                MultiplicationResult = Int32.Parse(MultiplicationResultString);
             }
 
             //Checking for missing digit
-            if(C.IndexOf('?')>=0){
-                int ans = a*b;
-                String str = ans.ToString();
-                int pos = C.IndexOf('?');
-                char res = str[pos];
-                r = res - '0';
-
-                if(C.IndexOf('?')==0){
-                    if(str.Equals(C.Substring(1))){
-                        r=-1;
-                    }
-                }
+            if(MultiplicationResultString.IndexOf('?')>=0){
+                MissingNumber = missingDigitInResult(FirstNumber,SecondNumber,MultiplicationResultString);
             }
-            if(A.IndexOf('?')>=0){
-                if(b != 0){
-                    int ans = c/b;
-                    String str = ans.ToString();
-                    int pos = A.IndexOf('?');
-                    char res = str[pos];
-                    r = res - '0';
 
-                    if(A.IndexOf('?')==0){
-                        if(str.Equals(A.Substring(1))){
-                        r=-1;
-                        }
-                    }
-                    if(A.IndexOf('?')==A.Length-1){
-                        if(str.Equals(A.Substring(0, A.Length-2))){
-                        r=-1;
-                        }else {
-                            String strA = A.Substring(0,A.Length-1);
-                            int Aa = Int32.Parse(strA);
-                            for(int x=0; x<10; x++){
-                                if(((Aa)*10+x)*b==c){
-                                    r = x;
-                                    break;
-                                }
-                                if((Aa*10+x)*b!=c){
-                                    r=-1;
-                                }
-                            }
-                        }
-                    }
-                }else{
-                    r=-1;
-                }
+            if(FirstNumberString.IndexOf('?')>=0){
+                MissingNumber = missingDigitInFirstNumber(MultiplicationResult, SecondNumber, FirstNumberString);
             }
-            if(B.IndexOf('?')>=0){
-                if(a != 0){
-                    int ans = c/a;
-                    String str = ans.ToString();
-                    int pos = B.IndexOf('?');
-                    char res = str[pos];
-                    r = res - '0';
 
-                    if(B.IndexOf('?')==0){
-                        if(str.Equals(B.Substring(1))){
-                        r=-1;
-                        }
-                    }
-                    if(B.IndexOf('?')==B.Length-1){
-                        if(str.Equals(B.Substring(0, B.Length-2))){
-                        r=-1;
-                        }else {
-                            String strB = B.Substring(0,B.Length-1);
-                            int Bb = Int32.Parse(strB);
-                            for(int x=0; x<10; x++){
-                                if(((Bb)*10+x)*a==c){
-                                    r = x;
-                                    break;
-                                }
-                                if((Bb*10+x)*a!=c){
-                                    r=-1;
-                                }
-                            }
-                        }
-                    }
-                }else{
-                    r=-1;
-                }
+            if(SecondNumberString.IndexOf('?')>=0){
+                MissingNumber = missingDigitInSecondNumber(MultiplicationResult, FirstNumber, SecondNumberString);
             }
-            return r;
-            throw new NotImplementedException();
+
+
+            return MissingNumber;
         }
+
+        public static int missingDigitInResult(int FirstNumber, int SecondNumber, string MultiplicationResultString){
+            int MissingNumber = -1;
+            int answer = FirstNumber*SecondNumber;
+                String answerString = answer.ToString();
+                int position = MultiplicationResultString.IndexOf('?');
+                char result = answerString[position];
+                MissingNumber = result - '0';
+                String verifyAnswer = MultiplicationResultString.Replace('?', result);
+
+                if(MultiplicationResultString.IndexOf('?')==0){
+                    if(answerString.Equals(MultiplicationResultString.Substring(1))){
+                        MissingNumber=-1;
+                    }
+                }
+                if(answerString.Equals(verifyAnswer)){
+                    MissingNumber = result - '0';
+                }else{
+                    MissingNumber = -1;
+                }
+
+                return MissingNumber;
+        }
+
+        public static int missingDigitInFirstNumber(int MultiplicationResult, int SecondNumber, string FirstNumberString){
+            int MissingNumber = -1;
+            if(SecondNumber != 0){
+                    int answer = MultiplicationResult/SecondNumber;
+                    String answerString = answer.ToString();
+                    int position = FirstNumberString.IndexOf('?');
+                    char result = answerString[position];
+                    MissingNumber = result - '0';
+
+                    if(FirstNumberString.IndexOf('?')==0){
+                        if(answerString.Equals(FirstNumberString.Substring(1))){
+                        MissingNumber=-1;
+                        }
+                    }
+                    if(FirstNumberString.IndexOf('?')==FirstNumberString.Length-1){
+                        if(answerString.Equals(FirstNumberString.Substring(0, FirstNumberString.Length-2))){
+                        MissingNumber=-1;
+                        }else {
+                            String firstNumberString = FirstNumberString.Substring(0,FirstNumberString.Length-1);
+                            int firstNumber = Int32.Parse(firstNumberString);
+                            for(int x=0; x<10; x++){
+                                if(((firstNumber)*10+x)*SecondNumber==MultiplicationResult){
+                                    MissingNumber = x;
+                                    break;
+                                }
+                                if((firstNumber*10+x)*SecondNumber!=MultiplicationResult){
+                                    MissingNumber=-1;
+                                }
+                            }
+                        }
+                    }
+                }else{
+                    MissingNumber=-1;
+                }
+                return MissingNumber;
+        }
+
+        public static int missingDigitInSecondNumber(int MultiplicationResult, int FirstNumber, string SecondNumberString){
+                int MissingNumber = -1;
+                if(FirstNumber != 0){
+                    int answer = MultiplicationResult/FirstNumber;
+                    String answerString = answer.ToString();
+                    int position = SecondNumberString.IndexOf('?');
+                    char result = answerString[position];
+                    MissingNumber = result - '0';
+
+                    if(SecondNumberString.IndexOf('?')==0){
+                        if(answerString.Equals(SecondNumberString.Substring(1))){
+                        MissingNumber=-1;
+                        }
+                    }
+                    if(SecondNumberString.IndexOf('?')==SecondNumberString.Length-1){
+                        if(answerString.Equals(SecondNumberString.Substring(0, SecondNumberString.Length-2))){
+                        MissingNumber=-1;
+                        }else {
+                            String secondNumberString = SecondNumberString.Substring(0,SecondNumberString.Length-1);
+                            int secondNumber = Int32.Parse(secondNumberString);
+                            for(int x=0; x<10; x++){
+                                if(((secondNumber)*10+x)*FirstNumber==MultiplicationResult){
+                                    MissingNumber = x;
+                                    break;
+                                }
+                                if((secondNumber*10+x)*FirstNumber!=MultiplicationResult){
+                                    MissingNumber=-1;
+                                }
+                            }
+                        }
+                    }
+                }else{
+                    MissingNumber=-1;
+                }
+                return MissingNumber;
+            }
+
+
     }
 }
